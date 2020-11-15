1a21b03c1abbacd3cfcf0845cabbb6dc45750147
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2020-11-15 14:30:39 -0700

    Update template_pipeline.groovy

--- template_pipeline.groovy
+++ template_pipeline.groovy
@@ -1,19 +1,13 @@
- // simple template
- // expand this and add more tests and more publishing into rpms etc
-
 pipeline {
-    // have your new agent do the builds tagged as rpm
-    // then this pipeline is run from the main jenkins instance
-    // routing jobs for this agent when label 'rpm'
     agent {
         label 'rpm'
     }
-
+    
 
     stages {
         stage('Build') {
             steps {
-                // pull from git
+                // Get some code from a GitHub repository
                 sh "cd /srv; rm -rf h2b2"
                 git 'https://github.com/jpegleg/h2b2'
                 sh "gcc h2b2.c -o h2b2 && cp h2b2 /srv/h2b2__built"
@@ -24,7 +18,7 @@ pipeline {
                 }
             }
         }
-        stage('Tests') {
+        stage('App Tests') {
             steps {
                 // test the program
                 sh "echo 123123123123 | /srv/h2b2__built > /srv/h2b2__tested"
@@ -38,13 +32,33 @@ pipeline {
         stage('Publish') {
             steps {
                 // make a tarball for pick up
-                sh "tar czvf /srv/h2b2_latest_qa.tgz /srv/h2b2__built && touch /srv/h2b2_pickup.lock"
+                sh "tar czvf /srv/h2b2_latest_qa.tgz /srv/h2b2__built /srv/workspace/h2b2_compiler_pipeline/ && touch /srv/h2b2_pickup.lock"
+                sh "cp /srv/h2b2_latest_qa.tgz /srv/rpmbuild/SOURCES/"
+                sh "cp /srv/workspace/h2b2_compiler_pipeline/h2b2.spec /srv/rpmbuild/SPECS/ "
+                sh "rm -f /usr/local/bin/h2b2"
+                sh "cd /srv/rpmbuild && rpmbuild -ba SPECS/h2b2.spec"
             }
             post {
                 success {
                     sh "ls /srv/h2b2_latest_qa.tgz || exit 1"
+                    sh "ls /usr/local/bin/h2b2 || exit 1"
+                    sh "cp /srv/rpmbuild/SRPMS/*.rpm /srv/ && touch /srv/h2b2_rpm_pickup.lock"
                 }
             }
-        }        
+        } 
+        stage('RPM Tests') {
+            steps {
+                // test the rpm
+                sh "rm -f /usr/local/bin/h2b2"
+                sh "ls -larth /srv/*.rpm | tail -n1 | awk '{print $9}' | xargs rpm -ivvv"
+            }
+            post {
+                success {
+                    sh "ls -larth /usr/local/bin/h2b2 || exit 1"
+                }
+            }
+        }     
+     
+     
     }
 }
